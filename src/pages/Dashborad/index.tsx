f57240d9e32b9215cd5cdf62f6a36f6135866e4e
devjean <jeancarloleall@gmail.com>
2023-04-19 22:56:20 -0300

    storage

--- src/pages/Dashborad/index.tsx
+++ src/pages/Dashborad/index.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
@@ -6,6 +6,22 @@ import { AuthContext } from "../../context/AuthContext";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+
+  const [store, setStore] = useState([]);
+
+  useEffect(() => {
+    if (store.length > 0) {
+      localStorage.setItem("store", JSON.stringify(store));
+    }
+  }, [store]);
+
+  // salvar no Local Storage
+  useEffect(() => {
+    const savedStore = localStorage.getItem("store");
+    if (savedStore) {
+      setStore(JSON.parse(savedStore));
+    }
+  }, []);
 
   return (
     <div className="container-dash">
